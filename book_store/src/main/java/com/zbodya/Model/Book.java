f1a5cc93d15fc7f0425462959b0696b3176c70e6
tempadmin2 <tempadmin2@Zbodya13>
2020-05-26 16:26:39 +0200

    Create a Book classs

--- book_store/src/main/java/com/zbodya/Model/Book.java
+++ book_store/src/main/java/com/zbodya/Model/Book.java
@@ -1,8 +1,22 @@
 package com.zbodya.Model;
 
+import java.io.File;
+import java.time.LocalDateTime;
+
 public class Book 
 {
 
 	private int ID;
 	
+	private LocalDateTime publishDate;
+	
+	private String title;
+	
+	private String describtion;
+	
+	private Publisher publisher;
+	
+	private Author author;
+	
+	private File image;
 }
